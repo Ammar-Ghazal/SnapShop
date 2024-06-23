90e8cdda82c741e54ed0fac1c2ac08fea65f8b60
ben-lu-uw <69722289+ben-lu-uw@users.noreply.github.com>
2024-06-23 02:52:42 -0400

    Add camera functionality (#1)

--- app/src/main/java/com/example/snapshop/MainActivity.kt
+++ app/src/main/java/com/example/snapshop/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.snapshop
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,12 +17,26 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.snapshop.databinding.ActivityMainBinding
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var imageCapture: ImageCapture
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,11 +45,8 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
+        binding.appBarMain.fab.setOnClickListener {openCamera()}
+        binding.appBarMain.cameraButton.setOnClickListener {takePicture()}
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -54,5 +70,69 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (permissions.contains(android.Manifest.permission.CAMERA)) {
+            if (grantResults[permissions.indexOf(android.Manifest.permission.CAMERA)] == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            } else {
+                Toast.makeText(this, "Camera required for functionality", Toast.LENGTH_LONG).show()
+            }
+
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun openCamera() {
+        // Check for permissions
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 100)
+        } else {
+            // Set up camera preview and lifecycle
+            val previewView = findViewById<View>(R.id.previewView) as PreviewView
+            val buttonView = findViewById<View>(R.id.cameraButton)
+            previewView.visibility = RelativeLayout.VISIBLE
+            buttonView.visibility = RelativeLayout.VISIBLE
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+            cameraProviderFuture.addListener({
+                imageCapture = ImageCapture.Builder().build()
+                val cameraProvider = cameraProviderFuture.get()
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                val preview = Preview.Builder().build()
+                preview.setSurfaceProvider(previewView.surfaceProvider)
+
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+                } catch (exception: Exception) {
+
+                }
+            }, ContextCompat.getMainExecutor(this))
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun takePicture() {
+        // Capture and save the image to storage
+        val imageCapture = imageCapture
+        val fileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMMdd_HHmmss")) + ".jpg"
+        val file = File(externalMediaDirs.first(), fileName)
+        val options = ImageCapture.OutputFileOptions.Builder(file).build()
+        Toast.makeText(this, file.absolutePath, Toast.LENGTH_LONG).show()
+
+        imageCapture.takePicture(options, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback{
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val previewView = findViewById<View>(R.id.previewView) as PreviewView
+                val buttonView = findViewById<View>(R.id.cameraButton)
+                previewView.visibility = RelativeLayout.GONE
+                buttonView.visibility = RelativeLayout.GONE
+                // can probably call gemini api func here
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+
+            }
+        })
     }
 }
